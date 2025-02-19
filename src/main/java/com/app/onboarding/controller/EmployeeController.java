53489c890575e9369af966cb8b5d6dc82bd5ff3a
Benson Kamau <muirubensonkamau@gmail.com>
2025-02-19 06:01:40 +0300

    swagger

--- src/main/java/com/app/onboarding/controller/EmployeeController.java
+++ src/main/java/com/app/onboarding/controller/EmployeeController.java
@@ -8,12 +8,14 @@ import com.app.onboarding.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -25,8 +27,10 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    // Create new employee
-    @PostMapping
+    // Create new
+
+
+    @PostMapping("/create")
     public ResponseEntity<?> onboardEmployee(@RequestBody EmployeeRequestDto employeeRequestDto) {
         try {
             EmployeeEntity createdEmployee = employeeService.createEmployee(employeeRequestDto);
